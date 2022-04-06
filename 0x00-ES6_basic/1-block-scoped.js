408ab0397a9b45451463ff884654fafbd082a4dc
jayse45 <ngyasi0@gmail.com>
2022-04-06 21:13:18 +0000

    correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-    if (trueOrFalse) {
-	//eslint-disable-next-line
+  if (trueOrFalse) {
+	// eslint-disable-next-line
 	const task = true;
-	//eslint-disable-next-line
+	// eslint-disable-next-line
 	const task2 = false;
   }
 
